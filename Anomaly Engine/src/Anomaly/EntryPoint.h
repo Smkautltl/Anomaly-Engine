b86df6cf30980e970768aa9dafb7698f5833da21
DESKTOP-PQ8QHKV\matt <Matthewcpye@hotmail.com>
2019-09-18 13:19:45 +0100

    Setup spdlog and started implementing it into the projects

--- Anomaly Engine/src/Anomaly/EntryPoint.h
+++ Anomaly Engine/src/Anomaly/EntryPoint.h
@@ -6,6 +6,11 @@ extern Anomaly::Application* Anomaly::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Anomaly::Log::Init();
+	AE_CORE_WARN("Initialized Log!");
+	int a = 10;
+	AE_INFO("Hello! VAR={0}", a);
+	
 	auto App = Anomaly::CreateApplication();
 	App->Run();
 	delete App;
